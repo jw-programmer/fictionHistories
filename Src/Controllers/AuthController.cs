c270f4d5ca762bf4209bab826df615742ed0e8db
Josias Wando Nobre de Queiroz <josiasn.queiroz@gmail.com>
2021-09-03 23:31:01 -0300

    Refresh endpoint

--- Src/Controllers/AuthController.cs
+++ Src/Controllers/AuthController.cs
@@ -1,9 +1,12 @@
 using System.Threading.Tasks;
 using System.Web;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Src.Authentication;
 using Src.Dtos;
+using Src.Models;
 using Src.Repositories;
 
 namespace Src.Controllers
@@ -14,11 +17,13 @@ namespace Src.Controllers
     {
         private readonly IConfiguration _config;
         private readonly AuthorRepository _repo;
+        private readonly UserManager<Author> _userManager;
 
-        public AuthController(IConfiguration config, AuthorRepository repo)
+        public AuthController(IConfiguration config, AuthorRepository repo, UserManager<Author> userManager)
         {
             _config = config;
             _repo = repo;
+            _userManager = userManager;
         }
 
         [HttpPost("login")]
@@ -34,8 +39,20 @@ namespace Src.Controllers
             var jwtToken = JwtService.GenerateJwtToken(user, _config.GetValue<string>("JWT:Secret"), await _repo.GetRolesByAuthorAsync(user));
 
             return Ok(new {
-                user = login,
                 token = jwtToken
+            });
+        }
+
+        [HttpPost("refresh")]
+        [Authorize]
+        public async Task<ActionResult<dynamic>> RefreshAsync()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+
+            var jwtRefreshToken =  JwtService.GenerateJwtToken(user, _config.GetValue<string>("JWT:Secret"), await _repo.GetRolesByAuthorAsync(user));
+
+            return Ok(new {
+                token = jwtRefreshToken
             });
         }
 
